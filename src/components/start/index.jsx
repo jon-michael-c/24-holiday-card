2c7a5371f158ddb1bb6546f83e3ca9e4f961e75f
jon-michael-c <jonmichaelcubano@gmail.com>
2024-12-06 10:12:02 -0500

    start changes

--- src/components/start/index.jsx
+++ src/components/start/index.jsx
@@ -1,8 +1,8 @@
-import { useEffect } from "react";
-import { gsap } from "gsap";
-import { TextPlugin } from "gsap/TextPlugin";
-import Tree from "../panels/Tree";
+import { useEffect, useRef } from "react";
 import Fireplace from "../panels/Fireplace";
+import Tree from "../panels/Tree";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 /*
  t1.to(".square-frame", {
@@ -21,10 +21,19 @@ import Fireplace from "../panels/Fireplace";
 
 export default function Start(props) {
   const { lotties } = props;
-  console.log(lotties);
+  const square = useRef(null);
+  const { contextSafe } = useGSAP();
+  const startAnim = contextSafe(() => {
+    const tl = gsap.timeline();
+    tl.to(square.current, {
+      duration: 1,
+      x: "-100%",
+      ease: "expo",
+    });
+  });
   useEffect(() => {}, []);
   return (
-    <div className="square-frame">
+    <div className="square-frame" ref={square}>
       <div className="content">
         <div className="content-inner h-full w-full flex flex-col gap-8 justify-center items-center">
           <div className="items relative scale-[4.2] top-[1181px] flex gap-8">
@@ -40,8 +49,9 @@ export default function Start(props) {
           </div>
           <div className="flex justify-center items-center w-full">
             <button
+              onClick={startAnim}
               id="start-button"
-              className="border-2 border-purple text-white px-4 py-2 cursor-pointer opacity-0"
+              className="border-2 border-purple text-white px-4 py-2 cursor-pointer opacity-0 uppercase text-2xl"
             >
               Click to Start
             </button>
