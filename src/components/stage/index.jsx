5cfe4a65ff736ac999b26baca5dbc31b679a0d27
jon-michael-c <jonmichaelcubano@gmail.com>
2024-12-08 00:19:50 -0500

    year phase

--- src/components/stage/index.jsx
+++ src/components/stage/index.jsx
@@ -9,6 +9,9 @@ function Stage() {
   const phase1 = useRef(null);
   const year1 = useRef(null);
   const year2 = useRef(null);
+  const yearLine = useRef(null);
+  const dot1 = useRef(null);
+  const dot2 = useRef(null);
   const { contextSafe } = useGSAP();
   gsap.registerPlugin(useGSAP);
 
@@ -48,12 +51,25 @@ function Stage() {
       ease: "none",
     });
     tl.to(phase1.current, {
-      duration: 1,
       x: "-25%",
-      ease: "expo",
+    });
+    tl.to(dot1.current, {
+      opacity: 1,
+      scale: 50,
+    });
+    tl.to(yearLine.current, {
+      height: "59%",
+    });
+    tl.to(year2.current, {
+      text: {
+        value: "2025",
+      },
+    });
+    tl.to(dot2.current, {
+      opacity: 1,
+      scale: 50,
     });
   });
-
   return (
     <>
       <div className="stage fixed top-0 left-0 w-full h-full opacity-0 invisible">
@@ -63,7 +79,20 @@ function Stage() {
               ref={text1}
               className="text-white font-extralight text-[120px]"
             ></p>
-            <div className="absolute top-[50%] left-1/2 text-white font-bold text-[120px]">
+            <div className="h-[325px] absolute top-[50%] left-1/2 text-white font-bold text-[120px] pl-12">
+              <div
+                ref={yearLine}
+                className="absolute top-[25%] -translate-x-1/2 left-0 h-[0%] w-2 bg-purple"
+              ></div>
+              <div
+                ref={dot1}
+                className="absolute top-[25%] left-0 -translate-x-1/2 h-[1px] w-[1px] rounded-full bg-purple opacity-0"
+              ></div>
+              <div
+                ref={dot2}
+                className="absolute bottom-[10%] left-0 -translate-x-1/2 h-[1px] w-[1px] rounded-full bg-purple opacity-0"
+              ></div>
+
               <p ref={year1}></p>
               <p ref={year2}></p>
             </div>
