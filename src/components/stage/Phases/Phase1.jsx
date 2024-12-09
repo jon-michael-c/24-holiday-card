988bf221261e5b337249386cf109321ac934fb9d
jon-michael-c <jon@jmcubano.com>
2024-12-09 12:29:12 -0500

    component refactor

---
+++ src/components/stage/Phases/Phase1.jsx
@@ -0,0 +1,35 @@
+// Phase1.js
+import React, { forwardRef } from "react";
+
+const Phase1 = forwardRef(
+  ({ text1, text2, year1, year2, yearLine, dot1, dot2, phase1 }, ref) => {
+    return (
+      <div className="phase-1 h-full w-full relative" ref={phase1}>
+        <p ref={text1} className="text-white font-extralight text-[120px]"></p>
+        <div className="h-[325px] absolute top-[50%] left-1/2 text-white font-bold text-[120px] pl-12">
+          <div
+            ref={yearLine}
+            className="absolute top-[25%] -translate-x-1/2 left-0 h-[0%] w-2 bg-purple"
+          ></div>
+          <div
+            ref={dot1}
+            className="absolute top-[25%] left-0 -translate-x-1/2 h-[1px] w-[1px] rounded-full bg-purple opacity-0"
+          ></div>
+          <div
+            ref={dot2}
+            className="absolute bottom-[10%] left-0 -translate-x-1/2 h-[1px] w-[1px] rounded-full bg-purple opacity-0"
+          ></div>
+          <div>
+            <p ref={year1}></p>
+            <p ref={year2}></p>
+          </div>
+          <div>
+            <p className="font-extralight text-nowrap" ref={text2}></p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default Phase1;
--- src/components/stage/index.jsx
+++ src/components/stage/index.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import useStageAnimation from "./useStageAnim";
+import Phase1 from "./Phases/Phase1";
 
 function Stage() {
   const refs = useRef({
@@ -20,39 +21,16 @@ function Stage() {
     <>
       <div className="stage fixed top-0 left-0 w-full h-full opacity-0 invisible">
         <div className="screen">
-          <div
-            className="phase-1 h-full w-full relative"
-            ref={(el) => (refs.current.phase1 = el)}
-          >
-            <p
-              ref={(el) => (refs.current.text1 = el)}
-              className="text-white font-extralight text-[120px]"
-            ></p>
-            <div className="h-[325px] absolute top-[50%] left-1/2 text-white font-bold text-[120px] pl-12">
-              <div
-                ref={(el) => (refs.current.yearLine = el)}
-                className="absolute top-[25%] -translate-x-1/2 left-0 h-[0%] w-2 bg-purple"
-              ></div>
-              <div
-                ref={(el) => (refs.current.dot1 = el)}
-                className="absolute top-[25%] left-0 -translate-x-1/2 h-[1px] w-[1px] rounded-full bg-purple opacity-0"
-              ></div>
-              <div
-                ref={(el) => (refs.current.dot2 = el)}
-                className="absolute bottom-[10%] left-0 -translate-x-1/2 h-[1px] w-[1px] rounded-full bg-purple opacity-0"
-              ></div>
-              <div>
-                <p ref={(el) => (refs.current.year1 = el)}></p>
-                <p ref={(el) => (refs.current.year2 = el)}></p>
-              </div>
-              <div>
-                <p
-                  className="font-extralight text-nowrap"
-                  ref={(el) => (refs.current.text2 = el)}
-                ></p>
-              </div>
-            </div>
-          </div>
+          <Phase1
+            phase1={(el) => (refs.current.phase1 = el)}
+            text1={(el) => (refs.current.text1 = el)}
+            year1={(el) => (refs.current.year1 = el)}
+            year2={(el) => (refs.current.year2 = el)}
+            yearLine={(el) => (refs.current.yearLine = el)}
+            dot1={(el) => (refs.current.dot1 = el)}
+            dot2={(el) => (refs.current.dot2 = el)}
+            text2={(el) => (refs.current.text2 = el)}
+          />
         </div>
       </div>
       <div
