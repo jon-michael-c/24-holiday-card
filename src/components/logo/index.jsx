cb18628d73be1d9a89a106f9df40e9d14c24d9d6
jon-michael-c <jon@jmcubano.com>
2024-12-16 15:13:26 -0500

    mobile logo

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -7,9 +7,9 @@ import ITZ from "./lotties/ITZ.json";
 function Logo() {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center relative">
-      <Lottie style={{ width: "auto", height: "200px" }} animationData={LEI} />
-      <Lottie style={{ width: "auto", height: "200px" }} animationData={BOW} />
-      <Lottie style={{ width: "auto", height: "200px" }} animationData={ITZ} />
+      <Lottie style={{ width: "auto", height: "auto" }} animationData={LEI} />
+      <Lottie style={{ width: "auto", height: "auto" }} animationData={BOW} />
+      <Lottie style={{ width: "auto", height: "auto" }} animationData={ITZ} />
     </div>
   );
 }
