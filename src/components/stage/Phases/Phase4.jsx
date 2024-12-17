9330f679bea0f325d4b1fc09c386f434287d4b68
jon-michael-c <jon@jmcubano.com>
2024-12-16 22:16:04 -0500

    phase 4 mobiel

--- src/components/stage/Phases/Phase4.jsx
+++ src/components/stage/Phases/Phase4.jsx
@@ -3,12 +3,12 @@ import React, { forwardRef } from "react";
 
 const Phase4 = forwardRef(({ phase4, ph4Text }, ref) => {
   return (
-    <div className="phase phase-3 h-full w-full relative" ref={phase4}>
+    <div className="phase phase-4 h-full w-full relative" ref={phase4}>
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
         <div className="flex items-start h-[300px] max-w-[750px]">
           <p
             ref={ph4Text}
-            className="dropcap dropcap-4 text-white h-[300px] w-full leading-[1.13] font-extralight text-[75px]"
+            className="dropcap dropcap-4 text-white h-[300px] w-full leading-[1.13] font-extralight text-[32px] sm:text-[75px]"
           ></p>
         </div>
       </div>
