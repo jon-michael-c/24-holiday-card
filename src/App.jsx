65c9ab0fbec22fb0be32e5d9df41d8000e9cfa6a
jon-michael-c <jonmichaelcubano@gmail.com>
2024-12-17 17:40:13 -0500

    intro anim

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import { TextPlugin } from "gsap/TextPlugin";
 import Loader from "./helpers/Loader";
 import Logo from "./components/logo";
 import Glow from "./components/filters/glow";
-import Sound from "./components/sound";
 gsap.registerPlugin(TextPlugin);
 
 function App() {
@@ -64,7 +63,7 @@ function App() {
         scale: 1,
         ease: ease,
       },
-      ">-=1.5"
+      ">-=0.85"
     );
 
     t1.to(".content-inner", {
@@ -72,12 +71,7 @@ function App() {
       scale: 1,
       ease: ease,
     });
-    t1.to("#start-button", {
-      duration: 0.65,
-      opacity: 1,
-      ease: ease,
-    });
-    t1.to(".title", {
+    t1.to(["#start-button", ".title"], {
       duration: 0.65,
       opacity: 1,
       ease: ease,
@@ -134,10 +128,10 @@ function App() {
             <div className="mx-auto max-w-[1200px] w-[80%] h-full mb-16 relative">
               <Logo />
             </div>
-            <p className="text-[20vw] sm:text-[12vw] leading-[1] font-[200] text-white">
+            <p className="text-[12vw] leading-[1] font-[200] text-white">
               {Math.round(loading, 2)}%
             </p>
-            <h1 className="loading-text text-[13vw] sm:text-[4vw] font-[200] text-white">
+            <h1 className="loading-text text-[4vw] font-[200] text-white">
               Magic Loading...
             </h1>
           </div>
